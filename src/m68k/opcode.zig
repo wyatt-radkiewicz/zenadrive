5f79c81de3feb595fb15d1a4ffb0f568587d1279
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2024-12-24 11:32:21 -0800

    Added line 0 instructions

--- src/m68k/opcode.zig
+++ src/m68k/opcode.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
+const expect = std.testing.expect;
 const ops = @import("ops.zig");
+const operand = @import("operand.zig");
 
 pub const Opcode = enum {
     ori_to_ccr,
@@ -98,22 +100,153 @@ pub const Opcode = enum {
                 return Opcode.decode_line_move(word);
             },
             inline else => |line| {
-                return @field(Opcode, std.fmt.comptimePrint("decode_line_{b}"), .{ line })(word);
-            }
+                return @field(Opcode, std.fmt.comptimePrint("decode_line_{b:0>4}", .{line}))(word);
+            },
         }
     }
 
-    fn decode_line_0(word: u16) Opcode {
+    fn decode_line_0000(word: u16) Opcode {
         if (ops.getbit(word, 8)) {
-            // Okay! we have either movep, or bit manipulation instructions with register operands
-            
+            if (operand.AddrMode.from_mode(ops.brange(u3, word, 3)) == operand.AddrMode.addr_reg) {
+                return Opcode.movep;
+            } else {
+                // Do bit instructions
+                return switch (ops.brange(u2, word, 6)) {
+                    0b00 => Opcode.btst,
+                    0b01 => Opcode.bchg,
+                    0b10 => Opcode.bclr,
+                    0b11 => Opcode.bset,
+                };
+            }
         } else {
             // Immidiate instructions
-            return switch (ops.brange(u3, word, 9)) {
-                0b000 => {
-                    if ()
-                }
+            switch (ops.brange(u3, word, 9)) {
+                0b000, 0b001, 0b101 => |ty| {
+                    // ORI instructions
+                    const enum_val = @intFromEnum(switch (ty){
+                        0b000 => Opcode.ori_to_ccr,
+                        0b001 => Opcode.andi_to_ccr,
+                        0b101 => Opcode.eori_to_ccr,
+                        else => unreachable,
+                    });
+                    if (operand.AddrMode.from_mode_xn(
+                        ops.brange(u3, word, 3),
+                        ops.brange(u3, word, 0),
+                    )) |addrmode| {
+                        if (addrmode == .imm) {
+                            return switch (ops.brange(u2, word, 6)) {
+                                0b00 => @enumFromInt(enum_val),
+                                0b01 => @enumFromInt(enum_val + 1),
+                                else => Opcode.illegal,
+                            };
+                        } else {
+                            return @enumFromInt(enum_val + 2);
+                        }
+                    } else {
+                        return Opcode.illegal;
+                    }
+                },
+                0b010 => return Opcode.subi,
+                0b011 => return Opcode.addi,
+                0b110 => return Opcode.cmpi,
+                0b100 => {
+                    return switch (ops.brange(u2, word, 6)) {
+                        0b00 => Opcode.btsti,
+                        0b01 => Opcode.bchgi,
+                        0b10 => Opcode.bclri,
+                        0b11 => Opcode.bseti,
+                    };
+                },
+                else => return Opcode.illegal,
             }
         }
     }
+    
+    fn decode_line_move(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_0100(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_0101(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_0110(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_0111(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1000(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1001(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1010(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1011(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1100(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1101(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1110(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
+    
+    fn decode_line_1111(word: u16) Opcode {
+        _ = word;
+        return Opcode.illegal;
+    }
 };
+
+test "line_0000" {
+    try expect(Opcode.decode(0x003C) == Opcode.ori_to_ccr);
+    try expect(Opcode.decode(0x007C) == Opcode.ori_to_sr);
+    try expect(Opcode.decode(0x0040) == Opcode.ori);
+    try expect(Opcode.decode(0x023C) == Opcode.andi_to_ccr);
+    try expect(Opcode.decode(0x027C) == Opcode.andi_to_sr);
+    try expect(Opcode.decode(0x0240) == Opcode.andi);
+    try expect(Opcode.decode(0x0440) == Opcode.subi);
+    try expect(Opcode.decode(0x0640) == Opcode.addi);
+    try expect(Opcode.decode(0x0A3C) == Opcode.eori_to_ccr);
+    try expect(Opcode.decode(0x0A7C) == Opcode.eori_to_sr);
+    try expect(Opcode.decode(0x0a40) == Opcode.eori);
+    try expect(Opcode.decode(0x0800) == Opcode.btsti);
+    try expect(Opcode.decode(0x0840) == Opcode.bchgi);
+    try expect(Opcode.decode(0x08c0) == Opcode.bseti);
+    try expect(Opcode.decode(0x0880) == Opcode.bclri);
+    try expect(Opcode.decode(0x0300) == Opcode.btst);
+    try expect(Opcode.decode(0x0340) == Opcode.bchg);
+    try expect(Opcode.decode(0x03c0) == Opcode.bset);
+    try expect(Opcode.decode(0x0380) == Opcode.bclr);
+    try expect(Opcode.decode(0x0188) == Opcode.movep);
+}
