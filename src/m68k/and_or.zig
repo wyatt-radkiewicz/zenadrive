a88b2f77f821d5dccd20f0e41311b386e7a48342
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-01-02 12:11:49 -0800

    Added or

--- src/m68k/and_or.zig
+++ src/m68k/and_or.zig
@@ -2,12 +2,15 @@ const std = @import("std");
 const enc = @import("cpu/enc.zig");
 const cpu = @import("cpu/cpu.zig");
 
+const Op = enum(u1) { @"or", @"and" };
 pub const Encoding = packed struct {
     ea: enc.EffAddr,
     size: enc.Size,
     dir: enc.OpDir,
     dn: u3,
-    line: enc.BitPattern(4, 0b1100),
+    line: enc.BitPattern(2, 0b00),
+    op: Op,
+    line_msb: enc.BitPattern(1, 0b1),
 };
 pub const Variant = packed struct {
     size: enc.Size,
@@ -30,7 +33,7 @@ pub fn match(comptime encoding: Encoding) bool {
     if (encoding.dir == .dn_ea_store_dn) {
         return enc.AddrMode.fromEffAddr(encoding.ea).? != .addr_reg;
     } else {
-        return switch(enc.AddrMode.fromEffAddr(encoding.ea).?) {
+        return switch (enc.AddrMode.fromEffAddr(encoding.ea).?) {
             .data_reg, .addr_reg, .imm, .pc_idx, .pc_disp => false,
             else => true,
         };
@@ -39,14 +42,19 @@ pub fn match(comptime encoding: Encoding) bool {
 pub fn run(state: *cpu.State, comptime args: Variant) void {
     // Compute effective addresses
     const instr: Encoding = @bitCast(state.ir);
-    
+
     // Set flags and store result
     if (args.dir == .dn_ea_store_dn) {
         const src_ea = cpu.EffAddr(args.size).calc(state, instr.ea);
-        const res = src_ea.load(state) & state.loadReg(.data, args.size, instr.dn);
+        const lhs = src_ea.load(state);
+        const rhs = state.loadReg(.data, args.size, instr.dn);
+        const res = switch (instr.op) {
+            .@"and" => lhs & rhs,
+            .@"or" => lhs | rhs,
+        };
         state.setLogicalFlags(args.size, res);
         state.storeReg(.data, args.size, instr.dn, res);
-    
+
         // Add processing time and fetch next instruction
         if (args.size == .long) {
             state.cycles += switch (src_ea) {
@@ -56,10 +64,15 @@ pub fn run(state: *cpu.State, comptime args: Variant) void {
         }
     } else {
         const dst_ea = cpu.EffAddr(args.size).calc(state, instr.ea);
-        const res = dst_ea.load(state) & state.loadReg(.data, args.size, instr.dn);
+        const lhs = dst_ea.load(state);
+        const rhs = state.loadReg(.data, args.size, instr.dn);
+        const res = switch (instr.op) {
+            .@"and" => lhs & rhs,
+            .@"or" => lhs | rhs,
+        };
         state.setLogicalFlags(args.size, res);
         dst_ea.store(state, res);
     }
-    
+
     state.ir = state.programFetch(enc.Size.word);
 }
