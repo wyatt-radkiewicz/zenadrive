7a62263baafa277dc88474b9de002d6a4d8f37bb
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-01-02 17:25:12 -0800

    Moved all testing to m68k.zig

--- src/m68k.zig
+++ src/m68k.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const enc = @import("m68k/cpu/enc.zig");
-const cpu_testing = @import("m68k/cpu/testing.zig");
 
 pub const cpu = @import("m68k/cpu/cpu.zig");
 
@@ -177,7 +176,7 @@ const decode_lut: [0x10000]u8 = compute_lut: {
 };
 
 test "Instructions" {
-    var bus = cpu_testing.Bus{};
+    var bus = Bus{};
     var state = cpu.State.init(&bus);
 
     inline for (instrs) |instr| {
@@ -256,3 +255,44 @@ const instrs = .{
     @import("m68k/tst.zig"),
     @import("m68k/unlk.zig"),
 };
+
+// Bus tester
+const Bus = struct {
+    bytes: [256]u8 = [_]u8{0} ** 256,
+
+    pub fn rd8(self: *const Bus, addr: u24) u8 {
+        if (addr > self.bytes.len - 1) return 0;
+        return self.bytes[addr];
+    }
+    pub fn rd16(self: *const Bus, addr: u24) u16 {
+        if (addr > self.bytes.len - 2) return 0;
+        return std.mem.readInt(u16, self.bytes[addr..][0..2], .big);
+    }
+    pub fn rd32(self: *const Bus, addr: u24) u32 {
+        if (addr > self.bytes.len - 4) return 0;
+        return std.mem.readInt(u32, self.bytes[addr..][0..4], .big);
+    }
+    pub fn wr8(self: *Bus, addr: u24, byte: u8) void {
+        if (addr > self.bytes.len - 1) return;
+        self.bytes[addr] = byte;
+    }
+    pub fn wr16(self: *Bus, addr: u24, word: u16) void {
+        if (addr > self.bytes.len - 2) return;
+        std.mem.writeInt(u16, self.bytes[addr..][0..2], word, .big);
+    }
+    pub fn wr32(self: *Bus, addr: u24, long: u32) void {
+        if (addr > self.bytes.len - 4) return;
+        std.mem.writeInt(u32, self.bytes[addr..][0..4], long, .big);
+    }
+};
+
+test "Bus Impl" {
+    const expect = std.testing.expect;
+    
+    var impl = Bus{};
+    var state = cpu.State.init(&impl);
+    
+    state.wrBus(enc.Size.byte, 4, 0xFF);
+    try expect(state.rdBus(enc.Size.byte, 4) == 0xFF);
+    try expect(state.rdBus(enc.Size.word, 4) == 0xFF00);
+}
